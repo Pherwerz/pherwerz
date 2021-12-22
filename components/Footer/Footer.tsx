6db8afa294bff3dc28e20ba1e5499e34a2a617af
Adebayo Fawaz <lordrisk@gmai.com>
2021-12-22 13:50:00 +0100

    latest

--- components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-dark text-white footer">
-      <div className="container">this is the footer</div>
-    </footer>
-  );
-};
-
-export default Footer;
--- pages/projects/[id].tsx
+++
@@ -1,5 +0,0 @@
-const projects = () => {
-  return <div>this is to test</div>;
-};
-
-export default projects;
